b0395b28c3ffa6b7915ab347b123760e5c43afe7
José Romero <jdromerov88@gmail.com>
2019-10-22 04:04:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 language_in_string =  programmer_hash[:dennis_ritchie][:languages].string
+		 language_in_string =  programmer_hash[:dennis_ritchie][:languages]
 	return language_in_string
 end
 
